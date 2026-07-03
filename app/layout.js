187801bf0bdba5a07524587cb301207a9d9cea4f
dahulk619 <dahulkisgreen@gmail.com>
2026-07-02 19:23:27 -0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,12 @@
+export const metadata = {
+  title: 'El PoKe pLuG Tracker',
+  description: 'Sealed Inventory Tracker',
+}
 
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body style={{ margin: 0 }}>{children}</body>
+    </html>
+  )
+}
